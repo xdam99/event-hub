fddff149c9a38646338103d21ee3f932bf6748b8
damienjss02100@gmail.com <damienjss02100@gmail.com>
2025-09-18 19:08:29 +0200

    Setup React + babel + webpack and create docker-compose

---
+++ client/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+  ],
+devServer: {
+    host: '0.0.0.0',    
+    port: 3000,
+    hot: true,
+    open: true,
+    static: './dist',
+},
+};
