0b001bc400f5cdfc8c0db3673610ea39c6142f46
damienjss02100@gmail.com <damienjss02100@gmail.com>
2025-10-15 10:02:25 +0200

    Update from TS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< Updated upstream
 import Header from './components/Header';
 
 export default function App() {
@@ -9,9 +8,4 @@ export default function App() {
       <h1>EventHub</h1>
     </div>
   );
-=======
-
-export default function App() {
-  return <h1>Hello React + Webpack + Babel !</h1>;
->>>>>>> Stashed changes
 }
