851266c51091f64db954d5a2798dae1d533ed356
damienjss02100@gmail.com <damienjss02100@gmail.com>
2025-12-19 15:49:31 +0100

    Change CheckPassword structural

--- eventhub_frontend/src/modules/register/components/Register.tsx
+++ eventhub_frontend/src/modules/register/components/Register.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { useState } from "react";
 
-import { CheckPassword } from "../../checkPassword/components/CheckPassword";
+import { CheckPassword } from "./checkPassword/components/CheckPassword";
 
 import "../ui/Register.css";
 
