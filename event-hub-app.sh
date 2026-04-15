ca72a514bf43d3295979890d3d2d23686e17a58a
damienjss02100@gmail.com <damienjss02100@gmail.com>
2026-04-15 14:37:00 +0200

    add shell for jenkins

---
+++ event-hub-app.sh
@@ -0,0 +1,6 @@
+echo "********************************"
+echo "CONSTRUCTION DE L'APPLICATION SUR LE PORT 3000"
+docker compose up -d --build
+echo "********************************"
+date
+echo "********************************"
