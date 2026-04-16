0effb8c5766fe22c31e6e6b0769329849aff4679
Damien J. <57198432+xdam99@users.noreply.github.com>
2026-04-16 10:17:10 +0200

    Remove '-d' flag from docker compose command

--- event-hub-app.sh
+++ event-hub-app.sh
@@ -1,6 +1,6 @@
 echo "********************************"
 echo "CONSTRUCTION DE L'APPLICATION SUR LE PORT 3000"
-docker compose up -d --build
+docker compose up --build
 echo "********************************"
 date
 echo "********************************"
