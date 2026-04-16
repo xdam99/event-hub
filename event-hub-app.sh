d4a03a3a2c100dec7fee20a1225de7eeede6950c
damienjss02100@gmail.com <damienjss02100@gmail.com>
2026-04-16 14:25:48 +0200

    Test for pipelines

--- event-hub-app.sh
+++ event-hub-app.sh
@@ -1,5 +1,4 @@
 echo "********************************"
-echo "CONSTRUCTION DE L'APPLICATION SUR LE PORT 3000"
 docker compose down
 docker compose up -d --build
 echo "********************************"
