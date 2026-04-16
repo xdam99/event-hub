6c29eb8af4d7c1d02dff69e2fb356a2860144caf
Damien J. <57198432+xdam99@users.noreply.github.com>
2026-04-16 10:30:02 +0200

    Fix docker compose command syntax

--- event-hub-app.sh
+++ event-hub-app.sh
@@ -1,6 +1,6 @@
 echo "********************************"
 echo "CONSTRUCTION DE L'APPLICATION SUR LE PORT 3000"
-docker compose up -f docker-compose.jenkins.yml --build -d
+docker compose up -- -d --build
 echo "********************************"
 date
 echo "********************************"
