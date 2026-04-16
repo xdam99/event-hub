45f8f2b48d1947dca96b711acb3522f679ce2b99
Damien J. <57198432+xdam99@users.noreply.github.com>
2026-04-16 10:21:28 +0200

    Update Docker command to use Jenkins compose file

--- event-hub-app.sh
+++ event-hub-app.sh
@@ -1,6 +1,6 @@
 echo "********************************"
 echo "CONSTRUCTION DE L'APPLICATION SUR LE PORT 3000"
-docker compose up --build
+docker compose up -f docker-compose.jenkins.yml --build -d
 echo "********************************"
 date
 echo "********************************"
